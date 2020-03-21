a4fb850024bf1daae25085de37ba0d200bc11346
simonestaffa <simonestaffa96@gmail.com>
2020-03-21 13:18:01 +0100

    Completed third query, and fixed first query

--- src/main/java/it/polimi/middleware/spark/car/accidents/CarAccidents.java
+++ src/main/java/it/polimi/middleware/spark/car/accidents/CarAccidents.java
@@ -8,6 +8,8 @@ import org.apache.spark.sql.types.StructType;
 import it.polimi.middleware.spark.utils.LogUtils;
 import it.polimi.middleware.spark.utils.Init;
 
+import static it.polimi.middleware.spark.utils.Init.*;
+
 public class CarAccidents {
 	public static void main(String[] args) {
 		LogUtils.setLogLevel();
@@ -34,9 +36,11 @@ public class CarAccidents {
 		corrected_ds.printSchema();
 
 		//Q1 Number of lethal accidents per week throughout the entire dataset
-		Init.first_query(corrected_ds);
+		first_query(corrected_ds);
 		//Q2 Number of accidents and percentage of number of deaths per contributing factor in the dataset.
-		Init.second_query(corrected_ds);
+		second_query(corrected_ds);
+		//Q3 Number of accidents and average number of lethal accidents per week per borough.
+		third_query(corrected_ds);
 		spark.close();
 
 	}
