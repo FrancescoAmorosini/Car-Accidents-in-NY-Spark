a85510a12d7be333d7d68caab3057093f6d8d212
simonestaffa <simonestaffa96@gmail.com>
2020-03-22 14:28:40 +0100

    Completed third query, and fixed first query

--- src/main/java/it/polimi/middleware/spark/car/accidents/CarAccidents.java
+++ src/main/java/it/polimi/middleware/spark/car/accidents/CarAccidents.java
@@ -1,5 +1,6 @@
 package it.polimi.middleware.spark.car.accidents;
 
+import org.apache.spark.SparkFiles;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -7,6 +8,8 @@ import org.apache.spark.sql.types.StructType;
 
 import it.polimi.middleware.spark.utils.LogUtils;
 import it.polimi.middleware.spark.utils.Init;
+
+import java.util.Date;
 
 import static it.polimi.middleware.spark.utils.Init.*;
 
@@ -19,29 +22,50 @@ public class CarAccidents {
 
 		final SparkSession spark = SparkSession
 				.builder() 
-				.master(master) 
+				.master("spark://Lim1.homenet.telecomitalia.it:7077")
 				.appName("Car Accidents in New York")
 				.getOrCreate();
 
 		final StructType mySchema = Init.getCarAccidentsSchema();
 
+		spark.sparkContext().addFile("file:/Users/simonestaffa/Desktop/MW/Car-Accidents-in-NY-Spark/files/NYPD_Motor_Vehicle_Collisions.csv");
+
+		final long startLoadingDataFromFile = new Date().getTime();
 		final Dataset<Row> ds = spark
 				.read()
 				.option("header", "true")
 				.option("delimiter", ",").option("inferSchema", "false").schema(mySchema)
-				.csv(filePath + "files/sample.csv");
-
+				.csv("file:/Users/simonestaffa/Desktop/MW/Car-Accidents-in-NY-Spark/files/NYPD_Motor_Vehicle_Collisions.csv");
+		final long endLoadingDataFromFile = new Date().getTime();
+		final long loadingDataFromFileTime = endLoadingDataFromFile - startLoadingDataFromFile;
 		//Filtering casualties and injuries mismatch
 		Dataset<Row> corrected_ds = Init.clearIncorrectValues(ds);
 		corrected_ds.printSchema();
 
 		//Q1 Number of lethal accidents per week throughout the entire dataset
+		final long startQuery1 = new Date().getTime();
 		first_query(corrected_ds);
+		final long endQuery1 = new Date().getTime();
+		final long query1 = endQuery1 - startQuery1;
+
 		//Q2 Number of accidents and percentage of number of deaths per contributing factor in the dataset.
+		final long startQuery2 = new Date().getTime();
 		second_query(corrected_ds);
+		final long endQuery2 = new Date().getTime();
+		final long query2 = endQuery2 - startQuery2;
+
 		//Q3 Number of accidents and average number of lethal accidents per week per borough.
+		final long startQuery3 = new Date().getTime();
 		third_query(corrected_ds);
+		final long endQuery3 = new Date().getTime();
+		final long query3 = endQuery3 - startQuery3;
+
 		spark.close();
+
+		System.out.println("---\nLOADING DATA FROM FILE: " + loadingDataFromFileTime);
+		System.out.println("---\nQUERY 1: " + query1);
+		System.out.println("---\nQUERY 2: " + query2);
+		System.out.println("---\nQUERY 3: " + query3);
 
 	}
 }
